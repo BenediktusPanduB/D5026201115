6396b7e666dd3970a3063ced88da79ee20b9d7a4
Benediktus Pandu Budhiwicaksono <88494183+BenediktusPanduB@users.noreply.github.com>
2021-11-18 15:34:13 +0700

    Belajar CRUD

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,13 @@ Route::get('ets',"ViewController@showETS");
 Route::get('dictionary',"ViewController@showDictionary");
 Route::post('answerDictionary',"ViewController@answer");
 
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
 
-
-
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
 
 
